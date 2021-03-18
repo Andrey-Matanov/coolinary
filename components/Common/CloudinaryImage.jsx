ad9f345804d0edaebf1e47b1dccde11ec065a383
Andrew Matanov <60537186+Andrey-Matanov@users.noreply.github.com>
2021-03-18 19:39:34 +0500

    removed unnecessary code

--- components/Common/CloudinaryImage.jsx
+++ components/Common/CloudinaryImage.jsx
@@ -4,19 +4,11 @@ import React from "react";
 const CloudinaryImage = ({ image }) => {
     return (
         <CloudinaryContext cloudName="coolinary">
-            <div className="App">
-                <section>
-                    {image.length ? (
-                        <Image publicId={image} fetch-format="auto" quality="auto" />
-                    ) : (
-                        <img
-                            src="https://via.placeholder.com/150x150"
-                            width={width}
-                            height={height}
-                        />
-                    )}
-                </section>
-            </div>
+            {image.length ? (
+                <Image publicId={image} fetch-format="auto" quality="auto" />
+            ) : (
+                <img src="https://via.placeholder.com/150x150" />
+            )}
         </CloudinaryContext>
     );
 };
